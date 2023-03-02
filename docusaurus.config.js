791a4116af0949dcd2975b64587d816246070be6
Jared Ledbetter <jared.ledbetter@outlook.com>
2023-03-02 18:04:49 -0500

    delete unused blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,11 +42,6 @@ const config = {
           path: 'docs',
           routeBasePath: 'docs',
           sidebarPath: require.resolve('./sidebars.js'),
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/carbondigitalus/veterandb-docs/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
