3e9a15f0c18191e1f5109a88c5749c79a1afd294
Jared Ledbetter <jared.ledbetter@outlook.com>
2023-03-02 18:04:59 -0500

    update name to Docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,7 +66,7 @@ const config = {
             type: 'doc',
             docId: 'intro',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Docs',
           },
           { to: '/blog', label: 'Blog', position: 'left' },
           {
