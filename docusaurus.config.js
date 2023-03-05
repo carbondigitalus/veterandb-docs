cbcd9636b1e1a2db1eeb5735548955574a517c07
Jared Ledbetter <jared.ledbetter@outlook.com>
2023-03-04 23:19:13 -0500

    update title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'VeteranDB',
   tagline: 'The last LZ you will ever need.',
+  title: 'Veteran Database',
   favicon: 'img/logo/icon-dark-bg.png',
 
   // Set the production url of your site here
