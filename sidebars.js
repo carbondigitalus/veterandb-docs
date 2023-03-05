31bcd5f72d43b96b28789e0baff8384b7b019ad9
Jared Ledbetter <jared.ledbetter@outlook.com>
2023-03-04 23:17:28 -0500

    delete unused links

--- sidebars.js
+++ sidebars.js
@@ -2,66 +2,11 @@
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
   // By default, Docusaurus generates a sidebar from the docs folder structure
-  api: [
-    'intro',
-    {
-      type: 'category',
-      label: 'Getting Started',
-      link: {
-        type: 'generated-index',
-      },
-      collapsed: false,
-      items: ['tutorial-basics/create-a-page'],
-    },
-  ],
-  docs: [
-    'intro',
-    {
-      type: 'category',
-      label: 'Getting Started',
-      link: {
-        type: 'generated-index',
-      },
-      collapsed: false,
-      items: ['tutorial-basics/create-a-page'],
-    },
-  ],
-  mobile: [
-    'intro',
-    {
-      type: 'category',
-      label: 'Getting Started',
-      link: {
-        type: 'generated-index',
-      },
-      collapsed: false,
-      items: ['tutorial-basics/create-a-page'],
-    },
-  ],
-  portal: [
-    'intro',
-    {
-      type: 'category',
-      label: 'Getting Started',
-      link: {
-        type: 'generated-index',
-      },
-      collapsed: false,
-      items: ['tutorial-basics/create-a-page'],
-    },
-  ],
-  website: [
-    'intro',
-    {
-      type: 'category',
-      label: 'Getting Started',
-      link: {
-        type: 'generated-index',
-      },
-      collapsed: false,
-      items: ['tutorial-basics/create-a-page'],
-    },
-  ],
+  api: ['intro'],
+  docs: ['intro'],
+  mobile: ['intro'],
+  portal: ['intro'],
+  website: ['intro'],
 };
 
 module.exports = sidebars;
